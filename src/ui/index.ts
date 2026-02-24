a59539534a1ddc68ee77d8b4fed5283572b78e1a
simplyyliam <simplyliam.dev@gmail.com>
2026-02-24 12:14:04 +0200

    ✨ feat(theme): add next-themes as a wrapper for layout

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,3 +1,4 @@
 
 export { default as Layout } from "./layout/layout";
 export { default as Home } from "./views/home";
+
--- src/ui/layout/layout.tsx
+++ src/ui/layout/layout.tsx
@@ -1,10 +1,17 @@
 import { Outlet } from "react-router-dom";
-
+import { ThemeProvider } from "../../styles";
 
 export default function Layout() {
-    return (
-        <div className="flex items-center justify-center w-screen h-screen">
-            <Outlet/>
-        </div>
-    )
+  return (
+    <div className="flex items-center justify-center w-screen h-screen">
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Outlet />
+      </ThemeProvider>
+    </div>
+  );
 }
