56fbf279ccf3b0718343131a6acb332813873abf
simplyyliam <simplyliam.dev@gmail.com>
2026-03-01 21:43:42 +0200

    ✨feat(ActivityBar): add activity bar with default icons seen in vscode

--- src/ui/components/editor/activitybar.tsx
+++ src/ui/components/editor/activitybar.tsx
@@ -1,13 +1,44 @@
+import { Blocks, BugPlay, File, GitBranch, Search } from "lucide-react";
+import type { HtmlHTMLAttributes } from "react";
 
+const IconWrapper: React.FC<HtmlHTMLAttributes<HTMLDivElement>> = ({
+  children,
+  className,
+  ...props
+}) => {
+  return (
+    <div
+      className={` flex items-center justify-center w-full h-9.25 ${className}`}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default function ActivityBar() {
-    return (
-        <div className="flex flex-col items-center justify-between h-full w-[37] bg-neutral-700">
-            {/* Top */}
-            <div className="flex flex-col items-center w-full h-fit">Top</div>
-            {/* Top */}
-            <div className="flex flex-col items-center w-full h-fit">Botom</div>
-            
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center justify-between h-full min-w-9.25 bg-neutral-700">
+      {/* Top */}
+      <div className="flex flex-col items-center w-full h-fit">
+          <IconWrapper>
+            <File size={20} />
+          </IconWrapper>
+        <IconWrapper>
+          <Search size={20} />
+        </IconWrapper>
+        <IconWrapper>
+          <GitBranch size={20} />
+        </IconWrapper>
+        <IconWrapper>
+          <BugPlay size={20} />
+        </IconWrapper>
+        <IconWrapper>
+          <Blocks size={20} />
+        </IconWrapper>
+      </div>
+      {/* Top */}
+      <div className="flex flex-col items-center w-full h-fit">Botom</div>
+    </div>
+  );
 }
--- src/ui/components/editor/editor.tsx
+++ src/ui/components/editor/editor.tsx
@@ -3,7 +3,7 @@ import StatusBar from "./statusbar";
 
 export default function Editor() {
   return (
-    <div className="flex flex-col bg-accent w-225 h-154.25 rounded-xl">
+    <div className="flex flex-col bg-accent w-225 h-154.25 rounded-xl overflow-hidden">
       <Content/>
       <StatusBar/>
     </div>
